8baae2c671e7fbdfdf9394bcb99156e97969f4ba
Devesh543 <Deveshlucky200@gmail.com>
2023-10-02 00:23:12 +0530

    renamed nav

---
+++ login/login_script.js
@@ -0,0 +1,10 @@
+
+function login() {
+    let pop = document.getElementById("login_page");
+    pop.style.visibility = "visible";
+}
+function login_reverse() {
+    let pop = document.getElementById("login_page");
+    pop.style.visibility = "hidden";
+}
+window.onload = login();
