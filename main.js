ce2f42932ce4eb23e76b76af4bea303be93f0642
Devesh Verma <deveshlucky200@gmail.com>
2024-09-01 18:30:09 +0530

    login

--- main.js
+++ main.js
@@ -111,7 +111,7 @@ function onSignIn(googleUser) {
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-
+alert(profile.getName());
   $("#name").text(profile.getName());
   $("#email").text(profile.getEmail());
   $("#image").attr('src',profile.getImageUrl());
