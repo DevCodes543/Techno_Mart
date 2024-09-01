737d3da3a0d57da60685aec506803f2bcf1548d0
Devesh Verma <deveshlucky200@gmail.com>
2024-09-01 18:20:46 +0530

    login

--- main.js
+++ main.js
@@ -98,3 +98,39 @@ login.addEventListener("click", (e) => {
 //     redirect_uri: "https://your.domain/code_callback_endpoint",
 //     state: "YOUR_BINDING_VALUE"
 //   });
+
+
+
+
+
+
+// Signin Google
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+  $("#name").text(profile.getName());
+  $("#email").text(profile.getEmail());
+  $("#image").attr('src',profile.getImageUrl());
+  $(".data").css("display","block");
+  $(".g-signin2").css("display","none")
+}
+
+
+
+/* <a href="#" onclick="signOut();">Sign out</a> */
+
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+
+      console.log('User signed out.');
+      alert("User signed out. !!")
+
+      $(".g-signin2").css("display","block")
+      $(".data").css('display',"none");
+    });
+  }
